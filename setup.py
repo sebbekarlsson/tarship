c7b57472a3e0146d38260c3607473914750e1ffd
ianertson <sebbekarlsson97@gmail.com>
2018-12-06 13:02:33 +0100

    ignore certain files when creating tar file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tarship',
-    version='1.0.1',
+    version='1.0.2',
     install_requires=[
         'paramiko',
         'scp'
--- tarship/utils.py
+++ tarship/utils.py
@@ -22,7 +22,11 @@ def get_instructions(source_dir):
 
 def make_tarfile(output_filename, source_dir):
     with tarfile.open(output_filename, "w:gz") as tar:
-        tar.add(source_dir, arcname=os.path.basename(source_dir))
+        tar.add(
+            source_dir,
+            arcname=os.path.basename(source_dir),
+            exclude=lambda x: '.git' in x or 'venv' in x or '.egg' in x
+        )
     tar.close()
 
     return tar
